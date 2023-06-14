01bbc8910e9f12213ebece338ac9028345b56208
YZX <1066437968@qq.com>
2023-06-14 21:04:40 +0800

    新增member会员模块

---
+++ member/src/main/java/com/cug/MemberApplication.java
@@ -0,0 +1,18 @@
+package com.cug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class MemberApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(MemberApplication.class, args);
+
+    }
+}
