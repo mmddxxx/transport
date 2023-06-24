b6ce0c3c583ef0b6b2b956419792e13eeda0ad8c
YZX <1066437968@qq.com>
2023-06-24 22:38:56 +0800

    4.16 封装请求参数和返回结果

---
+++ member/src/main/java/com/cug/mapper/MemberMapper.java
@@ -0,0 +1,35 @@
+package com.cug.mapper;
+
+import com.cug.domain.Member;
+import com.cug.domain.MemberExample;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface MemberMapper {
+    long countByExample(MemberExample example);
+
+    int deleteByExample(MemberExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Member record);
+
+    int insertSelective(Member record);
+
+    List<Member> selectByExample(MemberExample example);
+
+    Member selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByExample(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByPrimaryKeySelective(Member record);
+
+    int updateByPrimaryKey(Member record);
+}
