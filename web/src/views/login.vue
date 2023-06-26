b83aa33ab41fd8f6466dc397037e676bd4050930
YZX <1066437968@qq.com>
2023-06-26 22:19:09 +0800

    5.9 集成axios，npm install axios

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -39,53 +39,53 @@
 
 <script>
 import { defineComponent, reactive } from 'vue';
-// import axios from 'axios';
-// import { notification } from 'ant-design-vue';
-// import { useRouter } from 'vue-router'
-// import store from "@/store";
+import axios from 'axios';
+import { notification } from 'ant-design-vue';
+import { useRouter } from 'vue-router'
+import store from "@/store";
 
 export default defineComponent({
   name: "login-view",
   setup() {
-    // const router = useRouter();
+    const router = useRouter();
 
     const loginForm = reactive({
       mobile: '13000000000',
       code: '',
     });
 
-    // const sendCode = () => {
-    //   axios.post("/member/member/send-code", {
-    //     mobile: loginForm.mobile
-    //   }).then(response => {
-    //     let data = response.data;
-    //     if (data.success) {
-    //       notification.success({ description: '发送验证码成功！' });
-    //       loginForm.code = "8888";
-    //     } else {
-    //       notification.error({ description: data.message });
-    //     }
-    //   });
-    // };
+    const sendCode = () => {
+      axios.post("/member/member/sendCode", {
+        mobile: loginForm.mobile
+      }).then(response => {
+        let data = response.data;
+        if (data.success) {
+          notification.success({ description: '发送验证码成功！' });
+          loginForm.code = "8888";
+        } else {
+          notification.error({ description: data.message });
+        }
+      });
+    };
 
-    // const login = () => {
-    //   axios.post("/member/member/login", loginForm).then((response) => {
-    //     let data = response.data;
-    //     if (data.success) {
-    //       notification.success({ description: '登录成功！' });
-    //       // 登录成功，跳到控台主页
-    //       router.push("/welcome");
-    //       store.commit("setMember", data.content);
-    //     } else {
-    //       notification.error({ description: data.message });
-    //     }
-    //   })
-    // };
+    const login = () => {
+      axios.post("/member/member/login", loginForm).then((response) => {
+        let data = response.data;
+        if (data.success) {
+          notification.success({ description: '登录成功！' });
+          // 登录成功，跳到控台主页
+          router.push("/welcome");
+          store.commit("setMember", data.content);
+        } else {
+          notification.error({ description: data.message });
+        }
+      })
+    };
 
     return {
-      loginForm
-      // sendCode,
-      // login
+      loginForm,
+      sendCode,
+      login
     };
   },
 });
