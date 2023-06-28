e120a04f3669ecabd4d5c4d3037921c6903ba542
YZX <1066437968@qq.com>
2023-06-28 20:36:02 +0800

    7.5 使用线程本地变量存储会员信息

---
+++ member/src/main/java/com/cug/mytrain/config/SpringMvcConfig.java
@@ -0,0 +1,36 @@
+package com.cug.mytrain.config;
+
+import com.cug.mytrain.interceptor.LogInterceptor;
+import com.cug.mytrain.interceptor.MemberInterceptor;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 每个模块自定义使用哪些拦截器
+ */
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor logInterceptor;
+
+    @Resource
+    MemberInterceptor memberInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor);
+
+        //把拦截器加进来
+        // 路径不要包含context-path
+        registry.addInterceptor(memberInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/hello",
+                        "/member/sendCode",
+                        "/member/login"
+                );
+    }
+}
--- member/src/main/java/com/cug/mytrain/service/PassengerService.java
+++ member/src/main/java/com/cug/mytrain/service/PassengerService.java
@@ -2,6 +2,7 @@ package com.cug.mytrain.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import com.cug.mytrain.context.LoginMemberContext;
 import com.cug.mytrain.domain.Passenger;
 import com.cug.mytrain.mapper.PassengerMapper;
 import com.cug.mytrain.req.PassengerSaveReq;
@@ -18,6 +19,7 @@ public class PassengerService {
     public void save(PassengerSaveReq passengerSaveReq) {
         DateTime now = DateTime.now();
         Passenger passenger = BeanUtil.copyProperties(passengerSaveReq, Passenger.class);
+        passenger.setMemberId(LoginMemberContext.getId());
         passenger.setId(SnowUtil.getSnowflakeNextId());
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
