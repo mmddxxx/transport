83ce0839d89d281fc78e1442ccb3039781c018af
YZX <1066437968@qq.com>
2023-06-30 21:43:26 +0800

    10.2 增加batch定时调度模块

---
+++ batch/src/main/java/com/cug/mytrain/batch/BatchApplication.java
@@ -0,0 +1,22 @@
+package com.cug.mytrain.batch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class BatchApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(BatchApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BatchApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！！");
+        LOG.info("地址：\thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+    }
+}
