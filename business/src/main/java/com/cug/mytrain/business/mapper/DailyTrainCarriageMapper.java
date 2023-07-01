1763467968e53912306c3f59d7fa2155693537be
YZX <1066437968@qq.com>
2023-07-01 23:03:58 +0800

    11.5 生成每日车厢代码

---
+++ business/src/main/java/com/cug/mytrain/business/mapper/DailyTrainCarriageMapper.java
@@ -0,0 +1,33 @@
+package com.cug.mytrain.business.mapper;
+
+import com.cug.mytrain.business.domain.DailyTrainCarriage;
+import com.cug.mytrain.business.domain.DailyTrainCarriageExample;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DailyTrainCarriageMapper {
+    long countByExample(DailyTrainCarriageExample example);
+
+    int deleteByExample(DailyTrainCarriageExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(DailyTrainCarriage record);
+
+    int insertSelective(DailyTrainCarriage record);
+
+    List<DailyTrainCarriage> selectByExample(DailyTrainCarriageExample example);
+
+    DailyTrainCarriage selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") DailyTrainCarriage record, @Param("example") DailyTrainCarriageExample example);
+
+    int updateByExample(@Param("record") DailyTrainCarriage record, @Param("example") DailyTrainCarriageExample example);
+
+    int updateByPrimaryKeySelective(DailyTrainCarriage record);
+
+    int updateByPrimaryKey(DailyTrainCarriage record);
+}
