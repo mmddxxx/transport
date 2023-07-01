32da749b387d6e927cd181ade587a80482990cd8
YZX <1066437968@qq.com>
2023-07-01 14:45:49 +0800

    10.6 禁止任务并发执行

--- batch/src/main/java/com/cug/mytrain/batch/job/TestJob.java
+++ batch/src/main/java/com/cug/mytrain/batch/job/TestJob.java
@@ -1,13 +1,20 @@
 package com.cug.mytrain.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution
 public class TestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println("大成功");
     }
 }
