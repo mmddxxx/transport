f46a08b6c966ab687197bf7886fad7cdf81bdb93
YZX <1066437968@qq.com>
2023-07-01 23:11:19 +0800

    11.6 生成每日座位代码

---
+++ business/src/main/java/com/cug/mytrain/business/controller/DailyTrainSeatController.java
@@ -0,0 +1,39 @@
+package com.cug.mytrain.business.controller;
+
+import com.cug.mytrain.context.LoginMemberContext;
+import com.cug.mytrain.resp.CommonResp;
+import com.cug.mytrain.resp.PageResp;
+import com.cug.mytrain.business.req.DailyTrainSeatQueryReq;
+import com.cug.mytrain.business.req.DailyTrainSeatSaveReq;
+import com.cug.mytrain.business.resp.DailyTrainSeatQueryResp;
+import com.cug.mytrain.business.service.DailyTrainSeatService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/daily-train-seat")
+public class DailyTrainSeatController {
+
+    @Resource
+    private DailyTrainSeatService dailyTrainSeatService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody DailyTrainSeatSaveReq req) {
+        dailyTrainSeatService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<DailyTrainSeatQueryResp>> queryList(@Valid DailyTrainSeatQueryReq req) {
+        PageResp<DailyTrainSeatQueryResp> list = dailyTrainSeatService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        dailyTrainSeatService.delete(id);
+        return new CommonResp<>();
+    }
+
+}
---
+++ business/src/main/java/com/cug/mytrain/business/mapper/DailyTrainSeatMapper.java
@@ -0,0 +1,33 @@
+package com.cug.mytrain.business.mapper;
+
+import com.cug.mytrain.business.domain.DailyTrainSeat;
+import com.cug.mytrain.business.domain.DailyTrainSeatExample;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DailyTrainSeatMapper {
+    long countByExample(DailyTrainSeatExample example);
+
+    int deleteByExample(DailyTrainSeatExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(DailyTrainSeat record);
+
+    int insertSelective(DailyTrainSeat record);
+
+    List<DailyTrainSeat> selectByExample(DailyTrainSeatExample example);
+
+    DailyTrainSeat selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") DailyTrainSeat record, @Param("example") DailyTrainSeatExample example);
+
+    int updateByExample(@Param("record") DailyTrainSeat record, @Param("example") DailyTrainSeatExample example);
+
+    int updateByPrimaryKeySelective(DailyTrainSeat record);
+
+    int updateByPrimaryKey(DailyTrainSeat record);
+}
