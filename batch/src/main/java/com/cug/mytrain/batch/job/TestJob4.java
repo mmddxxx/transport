c3e12ce861aec10baec680e849a649242c3db365
YZX <1066437968@qq.com>
2023-07-02 23:19:18 +0800

    11.10 增加生成每日车站功能

--- batch/src/main/java/com/cug/mytrain/batch/job/TestJob4.java
+++ batch/src/main/java/com/cug/mytrain/batch/job/TestJob4.java
@@ -2,6 +2,7 @@ package com.cug.mytrain.batch.job;
 
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.RandomUtil;
 import com.cug.mytrain.batch.feign.BusinessFeign;
 import com.cug.mytrain.resp.CommonResp;
 import jakarta.annotation.Resource;
@@ -9,26 +10,28 @@ import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 import java.util.Date;
 
 @DisallowConcurrentExecution
 public class TestJob4 implements Job {
 
+    private static final Logger LOG = LoggerFactory.getLogger(DailyTrainJob.class);
+
     @Resource
     BusinessFeign businessFeign;
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-//        try {
-//            Thread.sleep(10000);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
+        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+        LOG.info("生成15天后的车次数据开始");
         Date date = new Date();
         DateTime dateTime = DateUtil.offsetDay(date, 15);//注意边界值
         Date offsetDate = dateTime.toJdkDate();
         CommonResp<Object> commonResp = businessFeign.genDaily(offsetDate);
-        System.out.println("Test Job111111");
+        LOG.info("生成15天后的车次数据结束，结果：{}", commonResp);
     }
 }
