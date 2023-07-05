89ba92be9e6acdce7457a7d44205b306adb2033a
YZX <1066437968@qq.com>
2023-07-05 15:46:01 +0800

    13.4 集成Nacos配置中心，实现动态刷新的功能

--- member/src/main/java/com/cug/mytrain/controller/TestController.java
+++ member/src/main/java/com/cug/mytrain/controller/TestController.java
@@ -1,15 +1,19 @@
 package com.cug.mytrain.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope  //动态刷新注入bean中的属性，以后想换配置中心，Java代码不需要动
 public class TestController {
 
-    @Value("${test.nacos")
+    @Value("${test.nacos}")
+    private String testNacos;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World123";
+        return String.format("Hello %s!", testNacos);
     }
 }
